20222341f488d5b674e55ff0ea8ec749dab995c2
dchaika <dchaika1990@gmail.com>
2020-07-30 10:51:48 +0300

    first commit

---
+++ src/components/FinishedQuiz/FinishedQuiz.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './FinishedQuiz.scss'
+
+const FinishedQuiz = props => {
+    return (
+        <div className={classes.FinishedQuiz}>
+            Finished
+        </div>
+    )
+}
+
+export default FinishedQuiz
